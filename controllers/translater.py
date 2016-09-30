9c3bd8f91014e4cdb324bf9ef84594ac9f62fb4f
tyrbonit <ityrbonit@yandex.ru>
2016-09-30 17:42:25 +0800

    Переводчик

--- controllers/translater.py
+++ controllers/translater.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
 def index(): return dict(message="hello from translater.py")
 
-def display_form():
-    record = 2#slovar(request.args(0)) or redirect(URL('index'))
-    form = SQLFORM(slovar, record)
+def selectform():
+    form = FORM(
+        INPUT(_name='slovo', _placeholder="Введите здесь текст для пословного перевода ...", _id="w2p_keywords", _class="form-control", _required=""),
+        INPUT(_type='submit', _value="Искать", _class="btn"),
+        _id="forma-poiska",
+        _action=""
+    )
+    rows=[]
     if form.process().accepted:
-        response.flash = 'form accepted'
-    elif form.errors:
-        response.flash = 'form has errors'
-    return dict(form=form)
+        if request.vars.id!=None:
+            sl=slovar(request.vars.id)
+            if sl!=None:request.vars.slovo=sl.slovo
+        if request.vars.slovo==None:return ""
+        if isinstance(request.vars.slovo,list):request.vars.slovo=request.vars.slovo[-1]
+        text=unicode(request.vars.slovo, 'utf-8')#Декодируем строку на всякий случай
+        rows=reshala(text)#Расчленение текста и выдача списка найденных слов в базе
+        #rows=db(slovar.id<20).select()
+    return dict(form=form,rows=rows)
+from gluon.tools import Crud
+crud = Crud(db)
 
-def selectforv():
-    rows=db(slovar.id<20).select(slovar.choiselist)
-    form=DIV(*[SELECT(row.choiselist) for row in rows],_class="col-md-4")
-    form.append(STYLE("select{-webkit-appearance: none;-moz-appearance: none;appearance: none;border:none;}select::-ms-expand{display: none;}"))
-    return dict(form=form)
+def display_form():
+    crud.settings.update_next=None
+    form=crud.update(slovar,request.vars.id)
+    return form
